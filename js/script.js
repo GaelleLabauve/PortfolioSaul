e12078266ff4b402c38d1bbd3ca39d175067a493
Gaelle Labauve <gaelle.labauve@etu.univ-smb.fr>
2024-10-25 13:00:45 +0200

    modif script

--- js/script.js
+++ js/script.js
@@ -20,3 +20,5 @@ const navLinks = document.querySelector(".nav-links")
 menuHamburger.addEventListener('click',()=>{
 navLinks.classList.toggle('mobile-menu')
 })
+
+//Rend actif le bon onglet dans le nav
