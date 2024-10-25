a44f82f1e541b190e5ab35889aba3e8b85c5509b
Gaelle Labauve <gaelle.labauve@etu.univ-smb.fr>
2024-10-25 23:01:35 +0200

    beacoup de changement

--- js/chargeIllustration.js
+++ js/chargeIllustration.js
@@ -9,15 +9,88 @@ const images = [
     'menu.png'
 ];
 
-const imageContainer = document.getElementById('galerie');
-
+const imageContainer = document.querySelector('.galerie');
+// let index = 1;
 images.forEach(image => {
+    const divElement = document.createElement('div');
+    divElement.classList.add('gallery-item');
+    // divElement.inert = index;
+    // index += 1;
     const imgElement = document.createElement('img');
     imgElement.src = `images/${image}`;
     imgElement.alt = image;
 
-    imageContainer.appendChild(imgElement);
+    divElement.appendChild(imgElement);
+    imageContainer.appendChild(divElement);
 });
+
+// const galleryItem  = document.querySelector('.gallery-item');
+
+// const lightBoxcontainer =  document.createElement('div');
+// const lightBoxContent =  document.createElement('div');
+// const lightBoxImg =  document.createElement('div');
+// const lightBoxPrev =  document.createElement('div');
+// const lightBoxNext =  document.createElement('div');
+
+// lightBoxcontainer.classList.add('lightbox')
+// lightBoxContent.classList.add('lightbox-content')
+// lightBoxPrev.classList.add('fa', 'fa-angle-left','lightbox-prev')
+// lightBoxNext.classList.add('fa', 'fa-angle-right','lightbox-next')
+
+// lightBoxcontainer.appendChild(lightBoxContent);
+// lightBoxContent.appendChild(lightBoxImg);
+// lightBoxContent.appendChild(lightBoxPrev);
+// lightBoxContent.appendChild(lightBoxNext);
+// document.body.appendChild(lightBoxcontainer);
+
+// index = 1;
+// function show(n){
+//     if (n > galleryItem.length){
+//         index = 1;
+//     } else if (n<1){
+//         index = galleryItem.length; 
+//     }
+
+//     let imageLocation = galleryItem[index-1].children[0].getAttribute("src");
+//     lightBoxImg.setAttribute("src", imageLocation);
+// }
+
+// function currentImage(){
+//     lightBoxcontainer.style.display="block";
+
+//     let imageIndex = parseInt(this.getAttribute("data-index"));
+//     show(index = imageIndex)
+// }
+
+// for (let i= 0; i<galleryItem.length; i++){
+//     galleryItem[i].addEventListener("click", currentImage);
+// }
+
+// function sliderImage(n){
+//     show(index += n);
+// }
+
+// function prevImage(){
+//     sliderImage(-1);
+// }
+
+// function nextImage(){
+//     sliderImage(1);
+// }
+
+// lightBoxPrev.addEventListener("click", prevImage);
+// lightBoxNext.addEventListener("click", nextImage);
+
+//close lightbox
+
+function closeLight(){
+    if(this === event.target){
+        lightBoxcontainer.style.display = "none";
+    }
+}
+
+
+
 
 
 /* ==================== */
@@ -29,15 +102,21 @@ const imgSlider = [
     'illu3.png'
 ]
 
-const imagebox = document.getElementById('image-box');
-const imagebg = document.getElementById('image-bg');
-
+const imagebox = document.querySelector('.image-box');
+const imagebg = document.querySelector('.image-bg');
+let first = true;
 imgSlider.forEach(image => {
     const radioElement = document.createElement('input');
     radioElement.type = 'radio'
     radioElement.name = 'image'
     radioElement.style.backgroundImage = `url(images/${image})`;
     radioElement.style.backgroundSize = 'cover';
+
+    if(first){
+        radioElement.checked = true;
+        imagebg.style.backgroundImage = radioElement.style.backgroundImage;
+        first = false;
+    }
 
     radioElement.addEventListener('change', () => {
         if (radioElement.checked) {
--- js/script.js
+++ js/script.js
@@ -8,17 +8,29 @@ fetch('assets/header.html')
 })
 .then(data => {
     document.getElementById('header').innerHTML = data;
+
+    //Gestion de l'appariton du menu en ecran petit
+    const menuHamburger = document.querySelector(".menu-burger")
+    const navLinks = document.querySelector(".nav-links")
+    
+    menuHamburger.addEventListener('click',()=>{
+    navLinks.classList.toggle('mobile-menu')
+    })
+
+    const currentUrl = window.location.pathname; // Récupérer le chemin de l'URL actuelle
+    const links = document.querySelectorAll('.nav-links a'); // Sélectionner tous les liens
+
+    links.forEach(link => {
+        if (link.getAttribute('href') === currentUrl.substring(1)) {
+            link.parentElement.classList.add('active'); // Ajouter la classe 'active' à l'élément parent <li>
+        } else {
+            link.parentElement.classList.remove('active'); // Retirer la classe 'active' des autres
+        }
+    });
+    
 })
 .catch(error => {
     console.error('Erreur:', error);
 });
-
-//Gestion de l'appariton du menu en ecran petit
-const menuHamburger = document.querySelector(".menu-burger")
-const navLinks = document.querySelector(".nav-links")
-
-menuHamburger.addEventListener('click',()=>{
-navLinks.classList.toggle('mobile-menu')
-})
 
 //Rend actif le bon onglet dans le nav
