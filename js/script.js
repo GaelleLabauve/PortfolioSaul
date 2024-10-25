a729eb779ba03e103cff5bd6e590d8a086333081
Gaelle Labauve <gaelle.labauve@etu.univ-smb.fr>
2024-10-25 11:29:06 +0200

    mise a jour arborescence des fichier

---
+++ js/script.js
@@ -0,0 +1,22 @@
+//met en place le header dans la page
+fetch('assets/header.html')
+.then(response => {
+    if (!response.ok) {
+        throw new Error('Erreur de chargement du header');
+    }
+    return response.text();
+})
+.then(data => {
+    document.getElementById('header').innerHTML = data;
+})
+.catch(error => {
+    console.error('Erreur:', error);
+});
+
+//Gestion de l'appariton du menu en ecran petit
+const menuHamburger = document.querySelector(".menu-burger")
+const navLinks = document.querySelector(".nav-links")
+
+menuHamburger.addEventListener('click',()=>{
+navLinks.classList.toggle('mobile-menu')
+})
