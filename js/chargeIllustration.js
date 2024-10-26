fbe4cafb3884cdf92c995a2d5e7e43e409367835
Gaelle Labauve <gaelle.labauve@etu.univ-smb.fr>
2024-10-26 12:13:10 +0200

    mise en place lightbox de la gallery

--- js/chargeIllustration.js
+++ js/chargeIllustration.js
@@ -10,86 +10,92 @@ const images = [
 ];
 
 const imageContainer = document.querySelector('.galerie');
-// let index = 1;
+const galleryItem = [];
+let index = 1;
 images.forEach(image => {
     const divElement = document.createElement('div');
     divElement.classList.add('gallery-item');
-    // divElement.inert = index;
-    // index += 1;
+    divElement.setAttribute('data-index', index);
+    index += 1;
     const imgElement = document.createElement('img');
     imgElement.src = `images/${image}`;
     imgElement.alt = image;
 
     divElement.appendChild(imgElement);
     imageContainer.appendChild(divElement);
+
+    galleryItem.push(divElement);
 });
 
-// const galleryItem  = document.querySelector('.gallery-item');
+//element for lightbox
+const lightBoxcontainer =  document.createElement('div');
+const lightBoxContent =  document.createElement('div');
+const lightBoxImg =  document.createElement('img');
+const lightBoxPrev =  document.createElement('div');
+const lightBoxNext =  document.createElement('div');
 
-// const lightBoxcontainer =  document.createElement('div');
-// const lightBoxContent =  document.createElement('div');
-// const lightBoxImg =  document.createElement('div');
-// const lightBoxPrev =  document.createElement('div');
-// const lightBoxNext =  document.createElement('div');
+//create classlist
+lightBoxcontainer.classList.add('lightbox')
+lightBoxContent.classList.add('lightbox-content')
+lightBoxPrev.classList.add('fa', 'fa-angle-left','lightbox-prev')
+lightBoxNext.classList.add('fa', 'fa-angle-right','lightbox-next')
 
-// lightBoxcontainer.classList.add('lightbox')
-// lightBoxContent.classList.add('lightbox-content')
-// lightBoxPrev.classList.add('fa', 'fa-angle-left','lightbox-prev')
-// lightBoxNext.classList.add('fa', 'fa-angle-right','lightbox-next')
+lightBoxcontainer.appendChild(lightBoxContent);
+lightBoxContent.appendChild(lightBoxImg);
+lightBoxContent.appendChild(lightBoxPrev);
+lightBoxContent.appendChild(lightBoxNext);
+document.body.appendChild(lightBoxcontainer);
 
-// lightBoxcontainer.appendChild(lightBoxContent);
-// lightBoxContent.appendChild(lightBoxImg);
-// lightBoxContent.appendChild(lightBoxPrev);
-// lightBoxContent.appendChild(lightBoxNext);
-// document.body.appendChild(lightBoxcontainer);
+index = 1;
 
-// index = 1;
-// function show(n){
-//     if (n > galleryItem.length){
-//         index = 1;
-//     } else if (n<1){
-//         index = galleryItem.length; 
-//     }
+function showLightBox(n){
+    if (n > galleryItem.length){
+        index = 1;
+    } else if (n<1){
+        index = galleryItem.length; 
+    }
 
-//     let imageLocation = galleryItem[index-1].children[0].getAttribute("src");
-//     lightBoxImg.setAttribute("src", imageLocation);
-// }
+    let imageLocation = galleryItem[index-1].children[0].getAttribute("src");
+    lightBoxImg.setAttribute("src", imageLocation);
+}
 
-// function currentImage(){
-//     lightBoxcontainer.style.display="block";
+function currentImage(){
+    lightBoxcontainer.style.display="block";
 
-//     let imageIndex = parseInt(this.getAttribute("data-index"));
-//     show(index = imageIndex)
-// }
+    let imageIndex = parseInt(this.getAttribute("data-index"));
+    showLightBox(index = imageIndex)
+}
 
-// for (let i= 0; i<galleryItem.length; i++){
-//     galleryItem[i].addEventListener("click", currentImage);
-// }
+console.log(galleryItem)
+for (let i= 0; i<galleryItem.length; i++){
+    console.log("entre")
+    galleryItem[i].addEventListener("click", currentImage);
+}
 
-// function sliderImage(n){
-//     show(index += n);
-// }
+function sliderImage(n){
+    showLightBox(index += n);
+}
 
-// function prevImage(){
-//     sliderImage(-1);
-// }
+function prevImage(){
+    sliderImage(-1);
+}
 
-// function nextImage(){
-//     sliderImage(1);
-// }
+function nextImage(){
+    sliderImage(1);
+}
 
-// lightBoxPrev.addEventListener("click", prevImage);
-// lightBoxNext.addEventListener("click", nextImage);
+lightBoxPrev.addEventListener("click", prevImage);
+lightBoxNext.addEventListener("click", nextImage);
 
 //close lightbox
 
-function closeLight(){
+function closeLightBox(){
     if(this === event.target){
         lightBoxcontainer.style.display = "none";
     }
 }
 
-
+lightBoxcontainer.addEventListener("click", closeLightBox);
 
 
 
@@ -105,6 +111,7 @@ const imgSlider = [
 const imagebox = document.querySelector('.image-box');
 const imagebg = document.querySelector('.image-bg');
 let first = true;
+
 imgSlider.forEach(image => {
     const radioElement = document.createElement('input');
     radioElement.type = 'radio'
