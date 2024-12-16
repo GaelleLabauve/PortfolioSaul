3e387045af83c2e143c63fecc894054be765e404
Gaelle Labauve <gaelle.labauve@etu.univ-smb.fr>
2024-12-16 19:09:27 +0100

    Ajout de l'inktober

---
+++ js/scriptInketober2024.js
@@ -0,0 +1,18 @@
+import { gallery, header } from "./utils.js";
+
+// Header
+header();
+
+// Gallery
+const images = [
+    'Inktober2024 (1).jpg',
+    'Inktober2024 (2).jpg',
+    'Inktober2024 (3).jpg',
+    'Inktober2024 (4).jpg',
+    'Inktober2024 (5).jpg',
+    'Inktober2024 (6).jpg',
+    'Inktober2024 (7).jpg',
+    'Inktober2024 (8).jpg',
+];
+
+gallery(images);
